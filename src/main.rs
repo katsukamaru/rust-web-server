070c1820b30424f09fce9014614fa4174766c255
shin katsumaru <s.katsumaru@loglass.co.jp>
2023-02-26 11:50:03 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use axum::{response::Html, routing::get, Router};
+use axum::{response::Html, routing::{get, post}, Router, Form};
 use std::net::SocketAddr;
+use serde::Deserialize;
 
 #[tokio::main]  // main関数を非同期関数にするために必要
 async fn main() {
@@ -8,7 +9,10 @@ async fn main() {
     }
     tracing_subscriber::fmt::init();
 
-    let app = Router::new().route("/", get(handler));
+    let app = Router::new()
+        .route("/", get(login))
+        .route("/sign_up", post(accept_form));
+    ;
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("listening on {}", addr);
@@ -18,6 +22,23 @@ async fn main() {
         .unwrap();
 }
 
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
+async fn login() -> Html<&'static str> {
+    let a = r#"
+    <form action="/sign_up" method="post">
+        <input type="text" name="username" placeholder="ユーザー名" />
+        <input type="password" name="password" placeholder="パスワード" />
+        <button type="submit">送信</button>
+    </form>"#;
+    Html(a)
+}
+
+#[derive(Deserialize)]  // Form<T>のTはserde::Deserializeを実装している必要がある
+struct SignUp {
+    username: String,
+    password: String,
+}
+
+async fn accept_form(form: Form<SignUp>) {
+    let sign_up: SignUp = form.0;
+    // DBに保存するなど
 }
